9a5716b89ef210f2e8020e72d28f4a3ea1db1fea
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-04-26 17:48:59 +0700

    :bento: Updated data sample

--- profile/script.py
+++ profile/script.py
@@ -30,62 +30,61 @@ PALETTE = {
         1: '#e67c73',
     },
 }
+AUTO_REPLACEMENT_LIST = [
+    # Tags
+    AutoReplacementData('<profile>', '[size=120][color={}]'.format(PALETTE['font'])),
+    AutoReplacementData('</profile>', '[/color][/size]'),
+    AutoReplacementData('<hr>', ('[color={}]{}[/color]').format(PALETTE['hr'], 60 * '━')),
+    # Text Variables
+    AutoReplacementData('<score_10>', '[color={}]10[/color]'.format(PALETTE['score'][10])),
+    AutoReplacementData('<score_9>', '[color={}]9[/color]'.format(PALETTE['score'][9])),
+    AutoReplacementData('<score_8>', '[color={}]8[/color]'.format(PALETTE['score'][8])),
+    AutoReplacementData('<score_7>', '[color={}]7[/color]'.format(PALETTE['score'][7])),
+    AutoReplacementData('<score_6>', '[color={}]6[/color]'.format(PALETTE['score'][6])),
+    AutoReplacementData('<score_5>', '[color={}]5[/color]'.format(PALETTE['score'][5])),
+    AutoReplacementData('<score_4>', '[color={}]4[/color]'.format(PALETTE['score'][4])),
+    AutoReplacementData('<score_3>', '[color={}]3[/color]'.format(PALETTE['score'][3])),
+    AutoReplacementData('<score_2>', '[color={}]2[/color]'.format(PALETTE['score'][2])),
+    AutoReplacementData('<score_1>', '[color={}]1[/color]'.format(PALETTE['score'][1])),
+    # BB Code Tag Altering
+    AutoReplacementData('[genre=', '[url=https://myanimelist.net/anime/genre/'),
+    AutoReplacementData('[/genre]', '[/url]'),
+    AutoReplacementData('[animelist=', '[url=https://myanimelist.net/animelist/'),
+    AutoReplacementData('[/animelist]', '[/url]'),
+    AutoReplacementData('[profile=', '[url=https://myanimelist.net/profile/'),
+    AutoReplacementData('[/profile]', '[/url]'),
+    AutoReplacementData('[people=', '[url=https://myanimelist.net/people/'),
+    AutoReplacementData('[/people]', '[/url]'),
+    AutoReplacementData('[review=', '[url=https://myanimelist.net/reviews.php?id='),
+    AutoReplacementData('[/review]', '[/url]'),
+    AutoReplacementData('[twitter=', '[url=https://twitter.com/'),
+    AutoReplacementData('[/twitter]', '[/url]'),
+    # Bullets Coloring
+    AutoReplacementData('◆10', '[color={}]◆[/color]'.format(PALETTE['score'][10])),
+    AutoReplacementData('◆9', '[color={}]◆[/color]'.format(PALETTE['score'][9])),
+    AutoReplacementData('◆8', '[color={}]◆[/color]'.format(PALETTE['score'][8])),
+    AutoReplacementData('◆7', '[color={}]◆[/color]'.format(PALETTE['score'][7])),
+    AutoReplacementData('◆6', '[color={}]◆[/color]'.format(PALETTE['score'][6])),
+    AutoReplacementData('◆5', '[color={}]◆[/color]'.format(PALETTE['score'][5])),
+    AutoReplacementData('◆4', '[color={}]◆[/color]'.format(PALETTE['score'][4])),
+    AutoReplacementData('◆3', '[color={}]◆[/color]'.format(PALETTE['score'][3])),
+    AutoReplacementData('◆2', '[color={}]◆[/color]'.format(PALETTE['score'][2])),
+    AutoReplacementData('◆1', '[color={}]◆[/color]'.format(PALETTE['score'][1])),
+    # Auto bullets coloring
+    AutoReplacementData('✦', '[color={}]✦[/color]'.format('#9579d1')),
+    AutoReplacementData('★', '[color={}]★[/color]'.format('#be9ddf')),
+    AutoReplacementData('✶', '[color={}]✶[/color]'.format('#ffa5d8')),
+    AutoReplacementData('❤', '[color={}]❤[/color]'.format('#92ddea')),
+    AutoReplacementData('✤', '[color={}]✤[/color]'.format('#7eb8da')),
+    AutoReplacementData('✔', '[b][color={}]✔[/color][/b]'.format('#48b07e')),
+    AutoReplacementData('✎', '[color={}]✎[/color]'.format('#ffcd42')),
+    AutoReplacementData('✒', '[color={}]✒[/color]'.format('#f2a14a')),
+]
 
 
 def main():
     ''' Main function '''
     print()
-
-    auto_replacement_list = [
-        # Tags
-        AutoReplacementData('<profile>', '[size=120][color={}]'.format(PALETTE['font'])),
-        AutoReplacementData('</profile>', '[/color][/size]'),
-        AutoReplacementData('<hr>', ('[color={}]{}[/color]').format(PALETTE['hr'], 60 * '━')),
-        # Text Variables
-        AutoReplacementData('<score_10>', '[color={}]10[/color]'.format(PALETTE['score'][10])),
-        AutoReplacementData('<score_9>', '[color={}]9[/color]'.format(PALETTE['score'][9])),
-        AutoReplacementData('<score_8>', '[color={}]8[/color]'.format(PALETTE['score'][8])),
-        AutoReplacementData('<score_7>', '[color={}]7[/color]'.format(PALETTE['score'][7])),
-        AutoReplacementData('<score_6>', '[color={}]6[/color]'.format(PALETTE['score'][6])),
-        AutoReplacementData('<score_5>', '[color={}]5[/color]'.format(PALETTE['score'][5])),
-        AutoReplacementData('<score_4>', '[color={}]4[/color]'.format(PALETTE['score'][4])),
-        AutoReplacementData('<score_3>', '[color={}]3[/color]'.format(PALETTE['score'][3])),
-        AutoReplacementData('<score_2>', '[color={}]2[/color]'.format(PALETTE['score'][2])),
-        AutoReplacementData('<score_1>', '[color={}]1[/color]'.format(PALETTE['score'][1])),
-        # Bullet Varibles
-        AutoReplacementData('◆10', '[color={}]◆[/color]'.format(PALETTE['score'][10])),
-        AutoReplacementData('◆9', '[color={}]◆[/color]'.format(PALETTE['score'][9])),
-        AutoReplacementData('◆8', '[color={}]◆[/color]'.format(PALETTE['score'][8])),
-        AutoReplacementData('◆7', '[color={}]◆[/color]'.format(PALETTE['score'][7])),
-        AutoReplacementData('◆6', '[color={}]◆[/color]'.format(PALETTE['score'][6])),
-        AutoReplacementData('◆5', '[color={}]◆[/color]'.format(PALETTE['score'][5])),
-        AutoReplacementData('◆4', '[color={}]◆[/color]'.format(PALETTE['score'][4])),
-        AutoReplacementData('◆3', '[color={}]◆[/color]'.format(PALETTE['score'][3])),
-        AutoReplacementData('◆2', '[color={}]◆[/color]'.format(PALETTE['score'][2])),
-        AutoReplacementData('◆1', '[color={}]◆[/color]'.format(PALETTE['score'][1])),
-        # BB Code Tag Altering
-        AutoReplacementData('[genre=', '[url=https://myanimelist.net/anime/genre/'),
-        AutoReplacementData('[/genre]', '[/url]'),
-        AutoReplacementData('[animelist=', '[url=https://myanimelist.net/animelist/'),
-        AutoReplacementData('[/animelist]', '[/url]'),
-        AutoReplacementData('[profile=', '[url=https://myanimelist.net/profile/'),
-        AutoReplacementData('[/profile]', '[/url]'),
-        AutoReplacementData('[people=', '[url=https://myanimelist.net/people/'),
-        AutoReplacementData('[/people]', '[/url]'),
-        AutoReplacementData('[review=', '[url=https://myanimelist.net/reviews.php?id='),
-        AutoReplacementData('[/review]', '[/url]'),
-        AutoReplacementData('[twitter=', '[url=https://twitter.com/'),
-        AutoReplacementData('[/twitter]', '[/url]'),
-        # Auto-coloring
-        AutoReplacementData('✦', '[color={}]✦[/color]'.format('#9579d1')),
-        AutoReplacementData('★', '[color={}]★[/color]'.format('#be9ddf')),
-        AutoReplacementData('✶', '[color={}]✶[/color]'.format('#ffa5d8')),
-        AutoReplacementData('❤', '[color={}]❤[/color]'.format('#92ddea')),
-        AutoReplacementData('✤', '[color={}]✤[/color]'.format('#7eb8da')),
-        AutoReplacementData('✔', '[b][color={}]✔[/color][/b]'.format('#48b07e')),
-        AutoReplacementData('✎', '[color={}]✎[/color]'.format('#ffcd42')),
-        AutoReplacementData('✒', '[color={}]✒[/color]'.format('#f2a14a')),
-    ]
 
     # v2
     converter = TextFileConverter(
@@ -95,7 +94,7 @@ def main():
     converter.convert(
         file_out_name=DIR + 'v2_fancy.txt',
         target_font_name='small_caps',
-        auto_replacement_list=auto_replacement_list,
+        auto_replacement_list=AUTO_REPLACEMENT_LIST,
         collapse='<<<<',
         tag=TagData('title', target_font_name='math_sans_bold_italic')
         
@@ -109,7 +108,7 @@ def main():
     converter.convert(
         file_out_name=DIR + 'v3_fancy.txt',
         target_font_name='small_caps',
-        auto_replacement_list=auto_replacement_list,
+        auto_replacement_list=AUTO_REPLACEMENT_LIST,
         collapse='<<<<',
         tag=TagData('title', target_font_name='math_sans_bold_italic')
     )
