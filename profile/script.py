364b463a7988422e3f837b864ec2c0de57d700b6
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-04-26 14:06:23 +0700

    :bento: Updated data sample

--- profile/script.py
+++ profile/script.py
@@ -14,9 +14,22 @@ def main():
     ''' Main function '''
     print()
 
+    # v2
     converter = TextFileConverter(
         '{}{}'.format(DIR, 'v2.txt'),
         '{}{}'.format(DIR, 'v2_fancy.txt')
+    )
+
+    converter.convert(
+        original_font='normal',
+        title_font='math_sans_bold_italic',
+        content_font='small_caps'
+    )
+
+    # v3
+    converter = TextFileConverter(
+        '{}{}'.format(DIR, 'v3.txt'),
+        '{}{}'.format(DIR, 'v3_fancy.txt')
     )
 
     converter.convert(
